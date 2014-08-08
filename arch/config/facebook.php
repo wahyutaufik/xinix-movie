5d2704868d94b13d3f299197a9a7750679adb1ac
Wahyu Taufik <wahyutaufik37@gmail.com>
2014-08-08 14:31:35 +0700

    Update facebook.php

--- arch/config/facebook.php
+++ arch/config/facebook.php
@@ -20,8 +20,8 @@
  */
 
 $config = array(
-        'facebook_app_id' => '236061656412034',
-        'facebook_api_secret' => '56e2c9a18ea9e083efcf7b3f4147efbd',
+        'facebook_app_id' => '',
+        'facebook_api_secret' => '',
         'facebook_default_scope' => 'email,publish_stream,manage_pages', // E.G 'read_stream,birthday,users_events,rsvp_event'
         'facebook_api_url' => 'https://graph.facebook.com/', // Just in case it changes.
 );
